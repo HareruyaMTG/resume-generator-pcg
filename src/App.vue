451e98dbf4317372e2a699dd6be1e1041868b43d
rmorioka <kysn.rm@gmail.com>
2021-08-13 09:52:09 +0900

    Quick start に従って Canvas を描画

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <v-stage :config="configKonva">
+      <v-layer>
+        <v-circle :config="configCircle"></v-circle>
+      </v-layer>
+    </v-stage>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      configKonva: {
+        width: 200,
+        height: 200,
+      },
+      configCircle: {
+        x: 100,
+        y: 100,
+        radius: 70,
+        fill: "red",
+        stroke: "black",
+        strokeWidth: 4,
+      },
+    };
+  },
 };
 </script>
 
