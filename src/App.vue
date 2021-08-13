f551b2c1334b8155f9bdf0671d6b0c745a183632
rmorioka <kysn.rm@gmail.com>
2021-08-13 13:18:44 +0900

    フォームの入力内容を formInput にまとめた

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,25 @@
       </konva-layer>
     </konva-stage>
     <v-form>
-      <v-text-field label="プレイヤーネーム" v-model="playerName" />
-      <v-text-field label="好きな色" v-model="favoriteColor" />
-      <v-text-field label="活動地域" v-model="activityArea" />
-      <v-text-field label="MTG歴" v-model="mtgHistory" />
+      <v-text-field label="プレイヤーネーム" v-model="formInput.playerName" />
+      <v-text-field label="好きな色" v-model="formInput.favoriteColor" />
+      <v-text-field label="活動地域" v-model="formInput.activityArea" />
+      <v-text-field label="MTG歴" v-model="formInput.mtgHistory" />
       <v-select
         label="要望&お知らせ"
-        v-model="notice"
+        v-model="formInput.notice"
         :items="noticeOptions"
         multiple
         chips
       />
       <v-select
         label="フォーマット"
-        v-model="playingFormat"
+        v-model="formInput.playingFormat"
         :items="playingFormatOptions"
         multiple
         chips
       />
-      <v-radio-group label="カテゴリ" v-model="playerCategory">
+      <v-radio-group label="カテゴリ" v-model="formInput.playerCategory">
         <v-radio label="初心者" value="初心者" />
         <v-radio
           label="カジュアル・エンジョイ"
@@ -33,9 +33,9 @@
         />
         <v-radio label="ガチ・競技" value="ガチ・競技" />
       </v-radio-group>
-      <v-textarea label="好きなカード" v-model="favoriteCard" />
-      <v-textarea label="好きなデッキ" v-model="favoriteDeck" />
-      <v-textarea label="フリースペース" v-model="freeSpace" />
+      <v-textarea label="好きなカード" v-model="formInput.favoriteCard" />
+      <v-textarea label="好きなデッキ" v-model="formInput.favoriteDeck" />
+      <v-textarea label="フリースペース" v-model="formInput.freeSpace" />
     </v-form>
   </v-app>
 </template>
@@ -45,11 +45,18 @@ export default {
   name: "App",
 
   data: () => ({
-    playerName: "",
-    favoriteColor: "",
-    activityArea: "",
-    mtgHistory: "",
-    notice: [],
+    formInput: {
+      playerName: "",
+      favoriteColor: "",
+      activityArea: "",
+      mtgHistory: "",
+      notice: [],
+      playingFormat: [],
+      playerCategory: "",
+      favoriteCard: "",
+      favoriteDeck: "",
+      freeSpace: "",
+    },
     noticeOptions: [
       "対戦したい",
       "大会に参加したい",
@@ -61,7 +68,6 @@ export default {
       "MOやってます",
       "コレクション自慢したい",
     ],
-    playingFormat: [],
     playingFormatOptions: [
       "スタンダード",
       "ヒストリック",
@@ -72,10 +78,6 @@ export default {
       "統率者",
       "その他",
     ],
-    playerCategory: "",
-    favoriteCard: "",
-    favoriteDeck: "",
-    freeSpace: "",
     stageConfig: {
       width: 800,
       height: 450,
@@ -88,7 +90,7 @@ export default {
   }),
   computed: {
     playerNameConfig() {
-      const text = this.playerName;
+      const text = this.formInput.playerName;
       const x = 190;
       const y = 97;
       return { ...this.fontSetting, text, x, y };
